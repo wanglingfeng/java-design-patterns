915cb6a8e2f5debac1b128f2ccdb59a9dc60e707
wanglingfeng <joseph.wlf@qq.com>
2017-03-01 16:49:45 +0800

    git modify comment

--- src/main/java/com/joseph/adapter/ObjectAdapter.java
+++ src/main/java/com/joseph/adapter/ObjectAdapter.java
@@ -6,7 +6,7 @@ import com.joseph.adapter.support.Targetable;
 /**
  * 对象的适配器模式
  * 核心思想：有一个Source类，拥有一个方法，待适配，目标接口时Targetable，
- *          通过ClassAdapter类，将Source的功能扩展到Targetable里
+ *          通过ObjectAdapter类，将Source的功能扩展到Targetable里
  *
  * Created by lfwang on 2016/12/9.
  */
