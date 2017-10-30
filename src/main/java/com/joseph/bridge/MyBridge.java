22c0b0b111b3f0a4a686078fbafb17e954af7a02
lfwang <joseph@thel.co>
2017-10-30 18:15:30 +0800

    remove duplicate implement funcation of bridge pattern

--- src/main/java/com/joseph/bridge/MyBridge.java
+++ src/main/java/com/joseph/bridge/MyBridge.java
@@ -4,8 +4,4 @@ package com.joseph.bridge;
  * Created by lfwang on 2016/12/9.
  */
 public class MyBridge extends Bridge {
-    @Override
-    public void execute() {
-        getDataBase().im();
-    }
 }
